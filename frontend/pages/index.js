30c9af547f525f26454d1f71eaa963b00dea0d3e
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2022-12-31 10:24:14 -0500

    Initial Setup

---
+++ frontend/pages/index.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+        <meta name="description" content="Blog" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="flex text-center">
+        <h1 className="text-3xl font-bold underline">
+          Hello World
+        </h1>
+        </div>
+      </main>
+    </>
+  );
+}
